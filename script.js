d7d68b13c19f8f524e9a6eb3600b829d27bd3b70
OctoLinkYT <138608589+OctoLinkYT@users.noreply.github.com>
2023-12-08 20:53:16 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
+  const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  let interval = null;
 
+  document.querySelector(".large-heading").onmouseover = event => {
+    let iteration = 0;
+    const targetElement = event.target;
+
+    clearInterval(interval);
+
+    interval = setInterval(() => {
+      targetElement.innerText = targetElement.innerText
+        .split("")
+        .map((letter, index) => {
+          if(index < iteration) {
+            return targetElement.dataset.value[index];
+          }
+
+          return letters[Math.floor(Math.random() * 26)];
+        })
+        .join("");
+
+      if(iteration >= targetElement.dataset.value.length){
+        clearInterval(interval);
+      }
+
+      iteration += 1 / 3;
+    }, 15);
+  }
